a32fe849960157190b0895c7e6cbe0cb92d232ad
Ganglion-17 <ganglion1717@gmail.com>
2023-11-12 13:38:00 +0500

    Initial adding

---
+++ lib/prism_checker_minitest.rb
@@ -0,0 +1,6 @@
+require "prism_checker_minitest/version"
+
+module PrismCheckerMinitest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prism_checker_minitest/version.rb
@@ -0,0 +1,3 @@
+module PrismCheckerMinitest
+  VERSION = "0.1.0"
+end
