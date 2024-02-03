477d6458e793e6fd298a9e48f8420eed6b9b1e60
Ganglion-17 <ganglion1717@gmail.com>
2024-02-03 13:19:44 +0500

    Test

--- spec/prism_checker_minitest_spec.rb
+++ spec/prism_checker_minitest_spec.rb
@@ -1,7 +1,29 @@
+# frozen_string_literal: true
+
 require 'prism_checker_minitest'
 
+class DummyAssert
+  include Minitest::Assertions
+
+  attr_accessor :assertions
+
+  def initialize
+    @assertions = 0
+  end
+end
+
+class DummyPage < SitePrism::Page
+  def text
+    'text'
+  end
+
+  def find(_)
+    nil
+  end
+end
+
 RSpec.describe Minitest::Assertions do
-  it "does something useful" do
-    expect(true).to eq(true)
+  it 'tests assert_page_like' do
+    expect(DummyAssert.new.assert_page_like(DummyPage.new, 'text')).to be_truthy
   end
 end
